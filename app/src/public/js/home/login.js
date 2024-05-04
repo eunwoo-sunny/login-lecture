c4625e5e77241cddb1de792e3bc766086331a4ea
blackcrown <eunwoosunny@gmail.com>
2024-05-04 14:16:44 +0900

    create fetch for request login info

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -11,6 +11,14 @@ function login() {
         id : id.value,
         pw : pw.value
     }
-
-    console.log('req', req)
+    
+    console.log('req :', req)
+    console.log('JSON.req : ', JSON.stringify(req))
+    fetch('/login', {
+        method :'POST', // 전달방식은 post 이다.
+        headers : {
+            "Content-Type" :"application/json" // json형태의 데이터를 전달하겠다.
+        },
+        body : JSON.stringify(req)
+    })
 }
