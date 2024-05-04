2331fc1e6d53a38ac6f1821eb7190f926382894a
blackcrown <eunwoosunny@gmail.com>
2024-05-04 13:52:29 +0900

    static folder

--- app/app.js
+++ app/app.js
@@ -10,8 +10,9 @@ const home = require('./src/routes/home/index')
 // app setting
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
+app.use(express.static(`${__dirname}/src/public`))
 
 // registering middleware;
 app.use('/', home); // use() is a method of registering middleware;
 
-module.exports = app;
+module.exports = app;                                                                                                                  
