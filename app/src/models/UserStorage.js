331fc2cc69712e5a459cdcf3023aeb5e80b81719
blackcrown <eunwoosunny@gmail.com>
2024-05-05 14:23:45 +0900

    login

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,14 +1,23 @@
 "use strict";
+const fs = require('fs').promises
+
 
 class UserStorage {
-    static #users = {
-        id : ['ejey','henry','sunny','shushu'],
-        pw : ['111','222','333','444'],
-        name : ['eunjung','hyunwoo','kisun','yuson']
+    static #getUserInfo(data, id) {
+        const users = JSON.parse(data) 
+        const idx = users.id.indexOf(id);
+        const usersKeys = Object.keys(users);  
+        const userInfo = usersKeys.reduce( (newUser, info) => {
+            newUser[info] = users[info][idx];
+            return newUser
+        }, {}) 
+        return userInfo;
     }
+
+    
     
     static getUsers(...fields) {
-        const users = this.#users;
+        // const users = this.#users;
         const newUsers = fields.reduce( (newUser, field) => {
             if(users.hasOwnProperty(field)) {
                 newUser[field] = users[field]
@@ -18,20 +27,20 @@ class UserStorage {
         return newUsers;
     }
 
-    static getUserInfo(id) {
-        const users = this.#users;
-        const idx = users.id.indexOf(id);
-        const usersKeys = Object.keys(users); // users의 키값들만 가져와서 배열로 만든다. [id,pw,name]
-        const userInfo = usersKeys.reduce( (newUser, info) => {
-            newUser[info] = users[info][idx];
-            return newUser
-        }, {})
-        return userInfo;
+    static getUserInfo(id) { 
+       return  fs.readFile('./src/databases/users.json')
+       .then(data => {
+           return this.#getUserInfo(data, id);
+       }) 
+       .catch(console.error)
+       // .catch(err => console.error(err)); -> 파라미터로 받아온값을 실행할 함수에 리턴할때는 위와 같이 생략가능
+        
     }
 
+    
 
     static save(userInfo) {
-        const users = this.#users;
+        // const users = this.#users;
         users.id.push(userInfo.id);
         users.name.push(userInfo.name);
         users.pw.push(userInfo.pw); 
